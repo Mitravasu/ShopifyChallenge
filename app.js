6889fa77e35fd3c10afc2db7ee1902bd7ba1d4c4
Mitravasu Prakash <43656604+Mitravasu@users.noreply.github.com>
2022-01-16 19:47:46 -0500

    [develop] Simplified example

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    // res.send('Hello, World!')
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.send('Hello, World!')
+    // res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
